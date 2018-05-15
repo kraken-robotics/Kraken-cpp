e8d216116ffec3dce2647b92e6f98ff459d333a0
kayou <pierre.bourre@gmail.com>
2018-05-15 22:21:52 +0200

    reformat Vector2D

--- sources/struct/Vector2D.h
+++ sources/struct/Vector2D.h
@@ -3,52 +3,35 @@
 
 #include <ostream>
 
-namespace kraken {
+namespace kraken
+{
 
-    class Vector2D {
+    class Vector2D
+    {
     public:
         Vector2D();
-
         Vector2D(float x, float y);
-
         Vector2D(const Vector2D &);
 
         Vector2D operator+(const Vector2D &rhs) const;
-
         Vector2D &operator+=(const Vector2D &rhs);
-
         Vector2D operator-(const Vector2D &rhs) const;
-
         Vector2D &operator-=(const Vector2D &rhs);
-
         Vector2D &operator*=(const float d);
-
         bool operator==(const Vector2D &rhs) const;
-
         float dot(const Vector2D &other) const;
 
         float squaredDistance(const Vector2D &other) const;
-
         float distance(const Vector2D &other) const;
-
         float distanceFast(const Vector2D &other) const;
-
         Vector2D &Ysym(bool do_symmetry);
-
         Vector2D rotate(float angle, Vector2D &rotation_center) const;
-
         void rotate(float angle, Vector2D &rotation_center);
-
         Vector2D &rotate(float angle);
-
         Vector2D &rotate(float cos, float sin);
-
         float getArgument() const;
-
         float getFastArgument() const;
-
         float squaredNorm() const;
-
         float norm() const;
 
         /**
@@ -59,13 +42,10 @@ namespace kraken {
         int distanceOctile(const Vector2D &other) const;
 
         float getX() const;
-
         float getY() const;
 
         void setX(float x);
-
         void setY(float y);
-
     public:
         /**
          * Returns true iff the segment (pointA1, pointA2) intersects the segment (pointB1, pointB2)
@@ -76,9 +56,7 @@ namespace kraken {
          * @return
          */
         static bool segmentIntersection(Vector2D &pointA1, Vector2D &pointA2, Vector2D &pointB1, Vector2D &pointB2);
-
         static Vector2D fromPolar(float radius, float angle);
-
     private:
         float x_;
         float y_;
