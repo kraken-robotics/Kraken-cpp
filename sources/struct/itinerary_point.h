72271ad9745184c5b244b9f6d2bd7a5315fc2f44
Kayou <pierre.bourre@lecortex.com>
2018-05-18 17:54:04 +0200

    add getter to itinerary points, set members as const

--- sources/struct/itinerary_point.h
+++ sources/struct/itinerary_point.h
@@ -20,14 +20,24 @@ namespace kraken
 
         bool operator==(const ItineraryPoint &rhs) const;
 
+        float getX() const;
+        float getY() const;
+        float getOrientation() const;
+        float getCurvature() const;
+        bool getGoingForward() const;
+        bool getStop() const;
+
     private:
-        Vector2D pos_;
-        float orientation_;
-        float curvature_;
-        float max_speed_;
-        float possible_speed_;
-        bool going_forward_;
-        bool stop_;
+        float compute_new_orientation(const float &orientation);
+
+    private:
+        const Vector2D pos_;
+        const float orientation_;
+        const float curvature_;
+        const float max_speed_;
+        const float possible_speed_;
+        const bool going_forward_;
+        const bool stop_;
 
 #if DEBUG
         friend std::ostream &operator<<(std::ostream &strm, const ItineraryPoint &v);
