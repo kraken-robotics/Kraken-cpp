29222ec1f73d62bf8c563a7fb8560a4f4f343103
PF <pierre-francois.gimenez@laposte.net>
2018-05-20 17:59:16 +0200

    Add override to circular_obstacle.h

--- sources/obstacles/circular_obstacle.h
+++ sources/obstacles/circular_obstacle.h
@@ -15,10 +15,10 @@ namespace kraken
     public:
         CircularObstacle(const Vector2D &pos, float radius);
         ~CircularObstacle() = 0;
-        bool isInObstacle(const Vector2D &pos) const;
-        float squaredDistance(const Vector2D &pos) const;
-        Vector2D* getExpandedConvexHull(float expansion, float longestAllowedLength) const;
-        bool isColliding(const Vector2D &point_a, const Vector2D &point_b) const;
+        bool isInObstacle(const Vector2D &pos) const override;
+        float squaredDistance(const Vector2D &pos) const override;
+        Vector2D* getExpandedConvexHull(float expansion, float longestAllowedLength) const override;
+        bool isColliding(const Vector2D &point_a, const Vector2D &point_b) const override;
 
         #if DEBUG
         friend std::ostream &operator<<(std::ostream &strm, const CircularObstacle &o);
