f64fec0d3459d79ee60c1b934df4b6be32be0cd7
PF <pierre-francois.gimenez@laposte.net>
2018-05-25 20:14:52 +0200

    Compound_obstacle in kraken namespace and operator==

--- sources/obstacles/compound_obstacle.cpp
+++ sources/obstacles/compound_obstacle.cpp
@@ -1,59 +1,74 @@
 #include "compound_obstacle.h"
 
 #include <limits>
+#include <typeinfo>
 
-kraken::CompoundObstacle::CompoundObstacle(const kraken::Vector2D &rotation_center,
-                                           std::vector<kraken::Obstacle> obstacles_list) :
-        Obstacle(rotation_center), obstacles_list_(std::move(obstacles_list))
+namespace kraken
 {
-}
-
-bool kraken::CompoundObstacle::isInObstacle(const kraken::Vector2D &pos) const
-{
-    for (auto const &o : obstacles_list_)
+    CompoundObstacle::CompoundObstacle(const kraken::Vector2D &rotation_center,
+                                       std::vector<kraken::Obstacle> obstacles_list) :
+            Obstacle(rotation_center), obstacles_list_(std::move(obstacles_list))
     {
-        if (o.isInObstacle(pos))
-            return true;
     }
 
-    return false;
-}
-
-float kraken::CompoundObstacle::squaredDistance(const kraken::Vector2D &pos) const
-{
-    float min = std::numeric_limits<float>::max();
-    for(auto const &o : obstacles_list_)
+    bool CompoundObstacle::isInObstacle(const kraken::Vector2D &pos) const
     {
-        min = std::min(min, o.squaredDistance(pos));
-        if(min == 0)
-            break;
+        for (auto const &o : obstacles_list_)
+        {
+            if (o.isInObstacle(pos))
+                return true;
+        }
+
+        return false;
     }
-    return min;
-}
 
-void kraken::CompoundObstacle::getExpandedConvexHull(const float &expansion, const float &longestAllowedLength,
-                                                     std::vector<kraken::Vector2D> &vector_2d_list) const
-{
-    for(const auto &o : obstacles_list_)
-        o.getExpandedConvexHull(expansion, longestAllowedLength, vector_2d_list);
-}
-
-bool kraken::CompoundObstacle::isColliding(const kraken::Vector2D &point_a, const kraken::Vector2D &point_b) const
-{
-    for(auto const &o : obstacles_list_)
+    float CompoundObstacle::squaredDistance(const kraken::Vector2D &pos) const
     {
-        if (o.isColliding(point_a, point_b))
-            return true;
+        float min = std::numeric_limits<float>::max();
+        for (auto const &o : obstacles_list_)
+        {
+            min = std::min(min, o.squaredDistance(pos));
+            if (min == 0)
+                break;
+        }
+        return min;
     }
-    return false;
-}
 
-bool kraken::CompoundObstacle::isColliding(const kraken::RectangularObstacle &obs) const
-{
-    for(auto const &o : obstacles_list_)
+    void CompoundObstacle::getExpandedConvexHull(const float &expansion, const float &longestAllowedLength,
+                                                 std::vector<kraken::Vector2D> &vector_2d_list) const
     {
-        if (o.isColliding(obs))
-            return true;
+        for (const auto &o : obstacles_list_)
+            o.getExpandedConvexHull(expansion, longestAllowedLength, vector_2d_list);
     }
-    return false;
+
+    bool CompoundObstacle::isColliding(const kraken::Vector2D &point_a, const kraken::Vector2D &point_b) const
+    {
+        for (auto const &o : obstacles_list_)
+        {
+            if (o.isColliding(point_a, point_b))
+                return true;
+        }
+        return false;
+    }
+
+    bool CompoundObstacle::isColliding(const kraken::RectangularObstacle &obs) const
+    {
+        for (auto const &o : obstacles_list_)
+        {
+            if (o.isColliding(obs))
+                return true;
+        }
+        return false;
+    }
+
+    bool CompoundObstacle::operator==(const Obstacle &rhs) const
+    {
+        if (!Obstacle::operator==(rhs))
+            return false;
+
+        if (typeid(*this) != typeid(rhs))
+            return false;
+
+        return static_cast<const CompoundObstacle &>(rhs).obstacles_list_ == obstacles_list_;
+    }
 }
