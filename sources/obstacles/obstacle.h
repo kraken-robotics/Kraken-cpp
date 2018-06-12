7177b01ba687046b7192ecae7fdae06865e630ff
Kayou <pierre.moltess@gmail.com>
2018-06-12 22:54:50 +0200

    Obstacle constructor is explicit

--- sources/obstacles/obstacle.h
+++ sources/obstacles/obstacle.h
@@ -11,7 +11,7 @@ namespace kraken
     class Obstacle
     {
     public:
-        Obstacle(const Vector2D &rotation_center) noexcept;
+        explicit Obstacle(const Vector2D &rotation_center) noexcept;
         virtual ~Obstacle() = default;
         virtual bool isInObstacle(const Vector2D &pos) const noexcept = 0;
         virtual float squaredDistance(const Vector2D &pos) const = 0;
