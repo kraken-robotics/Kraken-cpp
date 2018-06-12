5e8ca2bc518fa24dc9b98cd850014b6dd6059a9d
Kayou <pierre.moltess@gmail.com>
2018-06-12 21:58:15 +0200

    constexpr and noexcept for vector_2d

--- sources/struct/vector_2d.h
+++ sources/struct/vector_2d.h
@@ -2,8 +2,11 @@
 #define KRAKEN_VECTOR_2D_H
 
 #if DEBUG
+
 #include <ostream>
+
 #endif
+#include <cmath>
 
 namespace kraken
 {
@@ -11,41 +14,90 @@ namespace kraken
     class Vector2D
     {
     public:
-        Vector2D();
-        Vector2D(const float &x, const float &y);
+        constexpr Vector2D() : x_(0.f), y_(0.f)
+        {}
 
-        Vector2D operator+(const Vector2D &rhs) const;
-        Vector2D &operator+=(const Vector2D &rhs);
-        Vector2D operator-(const Vector2D &rhs) const;
-        Vector2D &operator-=(const Vector2D &rhs);
-        Vector2D &operator*=(const float &d);
-        Vector2D operator*(const float &d) const;
-        bool operator==(const Vector2D &rhs) const;
-        bool operator!=(const Vector2D &rhs) const;
-        float dot(const Vector2D &other) const;
+        constexpr Vector2D(const float &x, const float &y) : x_(x), y_(y)
+        {}
 
-        float squaredDistance(const Vector2D &other) const;
-        float distance(const Vector2D &other) const;
-        float distanceFast(const Vector2D &other) const;
-        Vector2D &Ysym(bool do_symmetry);
-        Vector2D rotate(float angle, const Vector2D &rotation_center) const;
-        void rotate(float angle, const Vector2D &rotation_center);
-        Vector2D &rotate(float angle);
-        Vector2D &rotate(float cos, float sin);
+        constexpr Vector2D operator+(const Vector2D &rhs) const noexcept
+        {
+            return {x_ + rhs.x_, y_ + rhs.y_};
+        }
+
+        constexpr Vector2D operator-(const Vector2D &rhs) const noexcept
+        {
+            return {x_ - rhs.x_, y_ - rhs.y_};
+        }
+
+        constexpr Vector2D operator*(const float &d) const noexcept
+        {
+            return {x_ * d, y_ * d};
+        }
+
+        constexpr bool operator==(const Vector2D &rhs) const noexcept
+        {
+            return x_ == rhs.x_ && y_ == rhs.y_;
+        }
+
+        constexpr bool operator!=(const Vector2D &rhs) const noexcept
+        {
+            return x_ != rhs.x_ || y_ != rhs.y_;
+        }
+
+        Vector2D &operator+=(const Vector2D &rhs) noexcept;
+        Vector2D &operator-=(const Vector2D &rhs) noexcept;
+        Vector2D &operator*=(const float &d) noexcept;
+
+        constexpr float dot(const Vector2D &other) const noexcept
+        {
+            return x_ * other.x_ + y_ * other.y_;
+        }
+
+        constexpr float squaredDistance(const Vector2D &other) const noexcept
+        {
+            return (x_ - other.x_) * (x_ - other.x_) + (y_ - other.y_) * (y_ - other.y_);
+        }
+
+        constexpr float distance(const Vector2D &other) const
+        {
+            return std::sqrt(squaredDistance(other));
+        }
+
+        float distanceFast(const Vector2D &other) const noexcept;
+        Vector2D &Ysym(bool do_symmetry) noexcept;
+        Vector2D rotate(float angle, const Vector2D &rotation_center) const noexcept;
+        void rotate(float angle, const Vector2D &rotation_center) noexcept;
+        Vector2D &rotate(float angle) noexcept;
+        Vector2D &rotate(float cos, float sin) noexcept;
         float getArgument() const;
         float getFastArgument() const;
-        float squaredNorm() const;
-        float norm() const;
+        constexpr float squaredNorm() const noexcept
+        {
+            return x_ * x_ + y_ * y_;
+        }
+
+        constexpr float norm() const
+        {
+            return std::sqrt(squaredNorm());
+        }
 
         /**
          * The distance is in μm !
          * @param other
          * @return
          */
-        int distanceOctile(const Vector2D &other) const;
+        int distanceOctile(const Vector2D &other) const noexcept;
 
-        float getX() const;
-        float getY() const;
+        constexpr float getX() const noexcept
+        {
+            return x_;
+        }
+
+        constexpr float getY() const noexcept
+        {
+            return y_;
+        }
 
         void setX(float x);
         void setY(float y);
@@ -58,8 +110,14 @@ namespace kraken
          * @param pointB2
          * @return
          */
-        static bool segmentIntersection(const Vector2D &pointA1, const Vector2D &pointA2, const Vector2D &pointB1, const Vector2D &pointB2);
-        static Vector2D fromPolar(float radius, float angle);
+        static bool segmentIntersection(const Vector2D &point_A1, const Vector2D &point_A2, const Vector2D &point_B1,
+                                        const Vector2D &point_B2) noexcept;
+
+        constexpr static Vector2D fromPolar(float radius, float angle) noexcept
+        {
+            return {std::cos(angle) * radius, std::sin(angle) * radius};
+        }
+
     protected:
         float x_;
         float y_;
